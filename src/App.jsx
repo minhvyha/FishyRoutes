05791f3e16ab0f3f08a04c7aa684d479ef6083d0
Duy <khanhduyn161@gmail.com>
2023-05-29 08:57:19 +1000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className='text-black'>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
