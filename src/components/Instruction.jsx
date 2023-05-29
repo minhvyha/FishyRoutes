6daf08048c92db7abb480f682e4a9935facfbee5
Duy <khanhduyn161@gmail.com>
2023-05-29 18:58:01 +1000

    Instruction

--- src/components/Instruction.jsx
+++ src/components/Instruction.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function Instruction() {
   return (
-    <div>Instruction</div>
-  )
+    <div className="h10">
+      <ul>
+        <li className="flex">
+          <i className="fa-solid flex fa-paw w-7 h-7 justify-center items-center"></i>
+          Start Node
+        </li>
+        <li className="flex">
+          <i className="fa-solid fa-fish w-7 h-7 flex justify-center items-center"></i>
+          Target Node
+        </li>
+        <li className="flex">
+          <div className="bg-gray-800 w-7 h-7 "></div>
+          Wall Node
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default Instruction
+export default Instruction;
