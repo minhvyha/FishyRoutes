81f6302fd8755dd6617dca1b6dd4b5de7912eae4
Minh Vy Ha <vyha0512@gmail.com>
2023-05-29 11:30:24 +1000

    update

---
+++ src/components/Box.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Box() {
+  return (
+    <div className='w-12 h-24 bg-white mb-2 border-black border-2 border-solid'>
+
+    </div>
+  )
+}
+
+export default Box
