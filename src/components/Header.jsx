743bcb51e7b7cba9803dc7a0bc5e50e201fbfac4
Minh Vy ha <minhvy.ha@outlook.com>
2023-05-29 18:57:54 +1000

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
-import React from "react";
-
+import React from 'react';
+import Select from 'react-select';
 function Header() {
+  const options = [
+    { value: 'dijkstra', label: `Dijkstra's Algorithm` },
+    { value: 'astar', label: 'A* Search' },
+
+  ];
   return (
-  <div>Header</div>
-    
-  )
+    <div className="bg-gray-600 px-8 py-4 text-white">
+      <div className="text-center text-3xl mb-2">Fishy Routes</div>
+      <div className="flex flex-row justify-between">
+        <Select placeholder="Select Algorithm" className="w-50 text-black" options={options} />
+        <button className="px-4 py-2 hover:bg-amber-500 duration-200 bg-amber-600 rounded-sm">Catch the fish!</button>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
