94d9ceb24718f07a352fb3b33b319cfe857484c3
Duy <khanhduyn161@gmail.com>
2023-05-30 12:02:39 +1000

    hover cursor instruction

--- src/components/Instruction.jsx
+++ src/components/Instruction.jsx
@@ -3,34 +3,38 @@ import React from "react";
 function Instruction() {
   return (
     <div className="p-1 bg-slate-200 flex">
-      <ul className="flex w-screen justify-start">
+      <ul className="flex justify-start">
         {/* Start Node icon */}
-        <li className="flex m-3">
+        <li
+          className="flex m-3 cursor-help"
+          data-tip="The start node where it first begin"
+          
+        >
           <i className="fa-solid flex fa-paw w-7 h-7 justify-center items-center mr-1 "></i>
-          Start Node
+          Cat Node
         </li>
 
         {/* Target Node icon */}
-        <li className="flex m-3">
+        <li className="flex m-3 cursor-help">
           <i className="fa-solid fa-fish w-7 h-7 flex justify-center items-center mr-1"></i>
-          Target Node
+          Fish Node
         </li>
 
         {/* Wall node icon */}
-        <li className="flex m-3">
+        <li className="flex m-3 cursor-help">
           <div className="bg-gray-800 w-7 h-7 mr-1"></div>
           Wall Node
         </li>
 
         {/* Visited Node icon */}
-        <li className="flex m-3">
+        <li className="flex m-3 cursor-help">
           <div className="bg-teal-400 w-7 h-7 mr-1"></div>
           <div className="bg-violet-600 w-7 h-7 mr-1"></div>
           Visited Nodes
         </li>
 
         {/* Shortest Path Node icon */}
-        <li className="flex m-3">
+        <li className="flex m-3 cursor-help">
           <div className="bg-yellow-300 w-7 h-7 mr-1"></div>
           Shortest-path Node
         </li>
