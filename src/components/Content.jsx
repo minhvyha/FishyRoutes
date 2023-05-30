f59715ca59fca3cd3947aa2fd3e30850e3bc1d86
Duy <khanhduyn161@gmail.com>
2023-05-30 18:53:47 +1000

    grid

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import Node from "./elements/Node";
 
 function Content() {
-  let grid = []
-  for (let row = 0; row < 10; row++) {
-    grid.push(<Node />)
+  let grid = [];
+  for (let row = 0; row < 60; row++) {
+    let temp = [];
+    for (let col = 0; col < 50; col++) {
+      temp.push(<Node />);
+    }
+    grid.push(temp);
   }
 
-  return <div className="w-screen">{grid}</div>;
+  return (
+    <div className="w-screen flex">
+      <div className="flex flex-wrap">{grid}</div>
+    </div>
+  );
 }
 
 export default Content;
--- src/components/elements/Node.jsx
+++ src/components/elements/Node.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Node() {
   return (
-    <div className='w-4 h-4 bg-white border-solid border-color border-black border '>
+    <div className='w-4 h-4 bg-white border-solid border-color border-black border'>
       
     </div>
   )
