202885f07c2832469859153550b570bcb6eac6ba
Minh Vy ha <minhvy.ha@outlook.com>
2023-05-30 18:39:36 +1000

    update

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import Node from "./elements/Node";
 
 function Content() {
-  let Nodes = <Node/>
+  let grid = []
   for (let row = 0; row < 10; row++) {
-    Nodes = <Node/>; 
+    grid.push(<Node />)
   }
 
-  return <div className="w-screen">{Nodes}</div>;
+  return <div className="w-screen">{grid}</div>;
 }
 
 export default Content;
