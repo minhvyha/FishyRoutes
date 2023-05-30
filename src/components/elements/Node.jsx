47b865c4b552d76f320fcf7c840c03c8756b19ba
Duy <khanhduyn161@gmail.com>
2023-05-30 18:37:47 +1000

    node

--- src/components/elements/Node.jsx
+++ src/components/elements/Node.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Node() {
   return (
-    <div className='w-4 h-4 bg-black'>
-
+    <div className='w-4 h-4 bg-white border-solid border-color border-black border '>
+      
     </div>
   )
 }
