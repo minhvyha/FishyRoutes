c1cb2f0f275b21c9c0829d9926ec5060728e547f
Minh Vy ha <minhvy.ha@outlook.com>
2023-06-01 18:35:37 +1000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import Instruction from './components/Instruction'
-import Content from './components/Content'
+import { useState } from 'react';
+import Header from './components/Header';
+import Instruction from './components/Instruction';
+import Content from './components/Content';
 
 export const MainPageContext = React.createContext();
 
-
 function App() {
-
-  let [selectedNode, setSelectedNode] = useState('wall')
-
-
+  let [grid, setGrid] = useState();
+  let [selectedNode, setSelectedNode] = useState('wall');
 
   return (
-    <>  
-    <MainPageContext.Provider value={{
-
-    }}>
-            <Header />
-      <Instruction />
-      <Content />
-    </MainPageContext.Provider>
-
+    <>
+      <MainPageContext.Provider
+        value={{
+          grid,
+          selectedNode,
+          setSelectedNode,
+          setGrid,
+        }}
+      >
+        <Header />
+        <Instruction />
+        <Content />
+      </MainPageContext.Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
