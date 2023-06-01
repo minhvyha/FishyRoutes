1f4b139b65c39f51d9532a376321e37f4792b137
Duy <khanhduyn161@gmail.com>
2023-06-01 13:32:52 +1000

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,25 @@ import Header from './components/Header'
 import Instruction from './components/Instruction'
 import Content from './components/Content'
 
+export const MainPageContext = React.createContext();
+
+
 function App() {
+
+  let [selectedNode, setSelectedNode] = useState('wall')
+
 
 
   return (
-    <>
-      <Header />
+    <>  
+    <MainPageContext.Provider value={{
+
+    }}>
+            <Header />
       <Instruction />
       <Content />
+    </MainPageContext.Provider>
+
     </>
   )
 }
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
 import Node from "./elements/Node";
+import Row from "./elements/Row";
 
 function Content() {
+  const windowSize = useRef([window.innerWidth, window.innerHeight]);
+  console.log(windowSize)
+  
   let grid = [];
-  for (let row = 0; row < 60; row++) {
+  for (let row = 0; row < 25; row++) {
     let temp = [];
     for (let col = 0; col < 50; col++) {
       temp.push(<Node />);
     }
-    grid.push(temp);
+    grid.push(<Row Nodes={temp} />);
   }
 
   return (
     <div className="w-screen flex">
-      <div className="flex flex-wrap">{grid}</div>
+      <div className="flex flex-col">{grid}</div>
     </div>
   );
 }
