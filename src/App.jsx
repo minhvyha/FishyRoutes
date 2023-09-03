21f6d8987cb661c85c13b3edeaf898be175ed30d
Minh Vy ha <minhvy.ha@outlook.com>
2023-09-03 15:08:25 +1000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,12 @@ function App() {
   let [grid, setGrid] = useState('0');
   let [selectedNode, setSelectedNode] = useState('cat');
 
+  let [catNode, setCatNode] = useState(undefined)
+  let [fistNode, setFishNode] = useState(undefined)
+  let [wallNode, setWallNode] = useState([])
+  let [visitedNode, setVisitedNode] = useState([])
+  let [shortestPath, setShortestPath] = useState([])
+
   return (
     <>
       <MainPageContext.Provider
