8c6d690cde6f132003dcbd39640db9acfca71ef5
Minh Vy ha <minhvy.ha@outlook.com>
2023-09-03 15:18:41 +1000

    update

--- src/components/elements/Node.jsx
+++ src/components/elements/Node.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { MainPageContext } from "../App";
+
 
 function Node({row, col}) {
+  
   return (
-    <div className='w-5 h-5 bg-white border-solid border-color border-black border' onClick={() => {}}>
+    <div className='w-5 h-5 bg-white border-solid border-color border-black border' draggable={false} onClick={() => {this.preventDefault()}}>
       
     </div>
   )
